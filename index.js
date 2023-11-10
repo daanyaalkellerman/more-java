b4c906bfdd58aef0afb61e53be11decb338de988
daanyaalkellerman <kellermandaanyaal2@gmail.com>
2023-11-10 14:47:16 +0200

    yes

---
+++ index.js
@@ -0,0 +1,42 @@
+
+// let driverLCNS = 18;
+// let learnerLCNS = 16;
+// let age = 15;
+
+// if(age>=driverLCNS){
+//     console.log('qualified for license')
+// }else {
+//     console.log('get older')
+// }
+// if(age>=learnerLCNS){
+//     console.log('qualified for learners')
+// }else{
+//     console.log('youre too young')
+// }
+
+// if (age>=18){
+//     console.log('you qualify for drivers')
+// } else if(age>=16){
+//     console.log('you qualify for learners')
+// }else{
+//     console.log('you dont qualify at all')
+// }
+
+document.getElementById('heading').innerText.textContent
+// get the text that the id is on
+document.getElementById('heading').innerHTML = 'change the content within'
+
+//innerhtml adding tags to the Element
+//innertext or textcontent to change the text
+// = overwrites
+// + adds words
+
+document.getElementsByClassName('para')
+//get the class text
+document.querySelectorAll('')
+//for everything with the same class or id
+document.querySelector("[]")
+// access everything attribute id class and elements
+// [] to get the data 
+document.querySelector('[data-numbers]').value
+//to get .value to get numbers
